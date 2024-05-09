162ec6104d82fe6a0c5272e0eecbdfce26778690
JJerome-NM <jjerome010@gmail.com>
2024-05-09 14:04:10 +0300

    Init

---
+++ src/main/java/com/jjerome/clearsolutionstestapp/ClearSolutionsTestAppApplication.java
@@ -0,0 +1,13 @@
+package com.jjerome.clearsolutionstestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClearSolutionsTestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClearSolutionsTestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jjerome/clearsolutionstestapp/ClearSolutionsTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjerome.clearsolutionstestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClearSolutionsTestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
